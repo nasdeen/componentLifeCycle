85f08986d1704bfa56f285ab82a6c51e81b8af65
Nas <nasdeen4best@gmail.com>
2021-03-24 00:15:31 +0000

    component life cycle

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ReviewMovie from "./NewYorkMovie/ReviewMovie";
+
+function App() {
+  return (
+    <div>
+      <ReviewMovie />
+    </div>
+  );
+}
+
+export default App;
